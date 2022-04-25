ebd0e69fa8a3f5c4545a7144b1e00f404d29b79f
JunayetTamim <junayettamim186@gmail.com>
2022-04-26 01:59:50 +0600

    server set

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,42 +1,38 @@
 import React from 'react';
-import './Orders.css'
+import { useNavigate } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import ReviewItem from '../ReviweItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
-import { Link } from 'react-router-dom';
+import './Orders.css';
 
 const Orders = () => {
-
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useCart(products);
-    const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
+    const [cart, setCart] = useCart();
+    const navigate = useNavigate();
+    const handleRemoveProduct = product => {
+        const rest = cart.filter(pd => pd._id !== product._id);
         setCart(rest);
-        removeFromDb(product.id);
+        removeFromDb(product._id);
     }
+
+
     return (
         <div className='shop-container'>
-            <div className="Review-container">
+            <div className="review-items-container">
                 {
-                    cart.map(product=><ReviewItem
-                        key={product.id}
-                        product = {product}
+                    cart.map(product => <ReviewItem
+                        key={product._id}
+                        product={product}
                         handleRemoveProduct={handleRemoveProduct}
                     ></ReviewItem>)
                 }
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
-                    <Link to="/shipment">
-                        <button>proceed Shipment </button>
-                    </Link>
+                    <button onClick={() => navigate('/shipment')}>Proceed Shipping </button>
                 </Cart>
-                
             </div>
         </div>
-            
     );
 };
 
